266347c50606d5c659c37a395f0f92b69414efd8
Krzysztof Bladowski <k.bladowski2012@gmail.com>
2019-03-02 12:47:20 +0100

    owca owca owca owca

---
+++ src/main/java/pl/teb/edukacja/owca/OwcaApplication.java
@@ -0,0 +1,13 @@
+package pl.teb.edukacja.owca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OwcaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OwcaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/teb/edukacja/owca/OwcaApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.teb.edukacja.owca;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OwcaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
